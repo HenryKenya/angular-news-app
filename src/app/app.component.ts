4775670db7d42337b78915fc827878ad9a6f84c8
HenryKenya <henry.onyango@outlook.com>
2020-03-29 22:16:15 +0300

    -add function for fetchData -add onclick listener to button -add directive on unordered list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'unit-testing-fundamentals';
+  isVisible: boolean = false;
+  
+  fetchData() {
+    this.isVisible = !this.isVisible
+  }
 }
